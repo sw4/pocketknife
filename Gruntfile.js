d1a016d2da9c3f3a12f851087fd06f7207d5c888
sw4 <samuel.wheeler@gmail.com>
2014-10-19 17:35:46 +0100

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -169,6 +169,8 @@ module.exports = function(grunt) {
 
     });
     grunt.loadNpmTasks('grunt-css');
+    // grunt.loadNpmTasks('grunt-contrib-csslint');
+    // grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-notify');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-concat');

77949529d7f7eac6109e2a15c6839c80fa2f659c
sw4 <samuel.wheeler@gmail.com>
2014-10-23 15:36:13 +0100

    Update checkbox.js

--- src/checkbox/checkbox.js
+++ src/checkbox/checkbox.js
@@ -1,26 +1,26 @@
 var pk = pk || {};
-(function (pk) {
-    pk.checkbox = function (opt) {
+(function(pk) {
+    pk.checkbox = function(opt) {
         var el = opt.element,
-            listeners = opt.listeners === undefined ? {} : opt.listeners,
+            //    listeners = opt.listeners === undefined ? {} : opt.listeners,
             inputValue = opt.value || el.getAttribute('value') || 0,
             inputLabel = opt.label || el.getAttribute('label') || el.innerHTML,
             inputDisabled = (opt.disabled || el.getAttribute('disabled')) ? 'disabled' : '',
             inputName = opt.name || el.getAttribute('name') || 'pk-checkbox-' + pk.getRand(1, 999),
             inputTabIndex = opt.tabindex || el.getAttribute('tabindex') || 0;
-        
+
         /*jshint multistr:true */
-        var str = "<label class='pk-checkbox' for='" + inputName+"'>\
-                <input type='checkbox' id='" + inputName + "' name='" + inputName + "' value='"+inputValue+"'  tabindex='" + inputTabIndex + "' />\
-                <span class='pk-label'>"+inputLabel+"</span>\
-            </label>";
-        el.innerHTML='';
+        var str = "<label class='pk-checkbox pk-noselect' for='" + inputName + "'>\
+		<input type = 'checkbox'  id = '" + inputName + "'  name = '" + inputName + "'  value = '" + inputValue + "'  tabindex = '" + inputTabIndex + "' / >\
+            <span class = 'pk-label' > " + inputLabel + " </span>\
+		</label>";
+        el.innerHTML = '';
         el = pk.replaceEl(el, str);
 
         var obj = {
             0: el,
             val: function(val) {
-                if (val === undefined) {                    
+                if (val === undefined) {
                     return inputValue;
                 }
                 pk.attribute(el.children[0], 'checked', Boolean(val));

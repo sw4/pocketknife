962447658053e3f5e66c5c358df57603c29f6284
sw4 <samuel.wheeler@gmail.com>
2014-10-29 12:42:50 +0000

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,19 @@
+npm install gulp  --save-dev
+npm install browser-sync gulp --save-dev
+npm install gulp-bump  --save-dev
+npm install gulp-autoprefixer  --save-dev
+npm install gulp-css-lint --save-dev
+npm install gulp-minify-css  --save-dev
+npm install gulp-jshint  --save-dev
+npm install gulp-concat  --save-dev
+npm install gulp-uglify  --save-dev
+npm install gulp-imagemin  --save-dev
+npm install gulp-notify  --save-dev
+npm install gulp-rename  --save-dev
+npm install gulp-cache  --save-dev
+npm install gulp-jsbeautifier --save-dev
+npm install gulp-less --save-dev
+npm install del --save-dev
+npm install gulp-header --save-dev
+npm install --save-dev gulp-load-plugins
+

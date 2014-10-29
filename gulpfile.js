e658ac79727cbe6c01186558d510589305444062
sw4 <samuel.wheeler@gmail.com>
2014-10-29 15:47:47 +0000

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,11 @@
 var gulp = require('gulp'),
-	pkg = require('./package.json')
-	less= require('gulp-less'),
-	concat = require('gulp-concat'),
-	csslint = require('gulp-csslint'),
-	autoprefixer = require('gulp-autoprefixer'),
-	rename = require('gulp-rename'),
-	minifyCSS = require('gulp-minify-css'),
-	jshint = require('gulp-jshint'),
-	uglify = require('gulp-uglify'),
-	header = require('gulp-header'),
-	bump = require('gulp-bump'),
+	pkg = require('./package.json'),
 	browserSync = require('browser-sync'),
-	prettify  = require('gulp-jsbeautifier');
-
+	plugins = require("gulp-load-plugins")({
+		pattern: ['gulp-*', 'gulp.*'],
+		replaceString: /\bgulp[\-.]/
+	});
+	
 	var banner = ['/**',
 	  ' * <%= pkg.title || pkg.name %>',
 	  ' * @version v<%= pkg.version %>',
@@ -22,37 +15,26 @@ var gulp = require('gulp'),
 	  ' */',
 	  ''].join('\n');
 
-// Auto load plugins with gulp-load-plugins https://www.npmjs.org/package/gulp-load-plugins
-/*
-var plugins = require('gulp-load-plugins')({
-    // pattern: 'gulp-*', // the glob to search for
-    // config: 'package.json', // where to find the plugins, by default  searched up from process.cwd() 
-    scope: ['dependencies', 'devDependencies', 'peerDependencies'], // which keys in the config to look within
-    replaceString: 'gulp-', // what to remove from the name of the module when adding it to the context
-    camelize: true, // if true, transforms hyphenated plugins names to camel case
-    lazy: true, // whether the plugins should be lazy loaded on demand
-});
-*/
  
 gulp.task('styles', ['bump'], function() {
   gulp.src('src/**/*.less', {base:'./'})
 	// Concat all LESS files
-    .pipe(concat(pkg.name+'.less'))
+    .pipe(plugins.concat(pkg.name+'.less'))
 	// Convert LESS to CSS
-	.pipe(less())
+	.pipe(plugins.less())
 	// Add vendor prefixes to CSS
-	.pipe(autoprefixer({
+	.pipe(plugins.autoprefixer({
 		browsers:['last 2 version', 'ie 8', 'ie 9'],
 		cascade:true
 	}))
 	// LINT CSS
-	.pipe(csslint())
+	.pipe(plugins.csslint())
 	// Minify CSS
-	.pipe(minifyCSS())
+	.pipe(plugins.minifyCss())
 	// Rename destination file
-	.pipe(rename(pkg.name+'.min.css'))
+	.pipe(plugins.rename(pkg.name+'.min.css'))
 	// Add banner
-	.pipe(header(banner, { pkg : pkg } ))
+	.pipe(plugins.header(banner, { pkg : pkg } ))
 	// Save output
     .pipe(gulp.dest('dist/'));
 	// Bump patch version
@@ -61,24 +43,24 @@ gulp.task('styles', ['bump'], function() {
 
 gulp.task('bump', function(){
   gulp.src('./package.json')
-	.pipe(bump({type:'patch'}))
+	.pipe(plugins.bump({type:'patch'}))
 	.pipe(gulp.dest('./'));
 });
 
 gulp.task('js', ['bump'],function() {
   gulp.src('src/**/*.js', {base: './'})
-  // .pipe(prettify())
-  // .pipe(gulp.dest('./'))
+	// .pipe(prettify())
+	// .pipe(gulp.dest('./'))
 	// Concat all JS files
-    .pipe(concat(pkg.name+'.js'))
+    .pipe(plugins.concat(pkg.name+'.js'))
 	// HINT JS
-	.pipe(jshint())
+	.pipe(plugins.jshint())
 	// Minify JS
-	.pipe(uglify())
+	.pipe(plugins.uglify())
 	// Rename destination file 
-	.pipe(rename(pkg.name+'.min.js'))
+	.pipe(plugins.rename(pkg.name+'.min.js'))
 	// Add banner
-	.pipe(header(banner, { pkg : pkg } ))
+	.pipe(plugins.header(banner, { pkg : pkg } ))
 	// Save output	
     .pipe(gulp.dest('dist/'));
 	// Bump patch version

6ad705224d020635fe7335a92b8a179d0ffbf957
sw4 <samuel.wheeler@gmail.com>
2014-11-04 15:56:48 +0000

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -100,10 +100,6 @@ gulp.task('git:commit', function () {
 
 gulp.task('document', function () {
 	plugins.run('yuidoc').exec();
-   gulp.src('_docs_main.css', {base:'./'})
-	.pipe(plugins.rename('main.css'))
-	.pipe(plugins.wait(5000))
-   .pipe(gulp.dest('docs/assets/css/'));	
 });
 
 gulp.task('default', ['watch']);

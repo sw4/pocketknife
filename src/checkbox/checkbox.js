68cea6bd2f576e8757fbf3bbbf084ae0da931e13
sw4 <samuel.wheeler@gmail.com>
2014-11-05 11:07:11 +0000

    Update checkbox.js

--- src/checkbox/checkbox.js
+++ src/checkbox/checkbox.js
@@ -1,100 +1,86 @@
 var pk = pk || {};
+
 /**
-Create a new accordian component from an unordered list element `<ul>` with the below structure:
+Create a new checkbox control
 
 HTML:
 
-	<ul id='accordian'>
-        <li>
-            <h3 class='pk-content-header'>Content header</h3>
-            <div class='pk-content'>
-				Content to collapse
-			</div>
-        </li>
-        <li>
-            <h3 class='pk-content-header'>Content header</h3>
-            <div class='pk-content'>
-				Content to collapse
-			</div>
-        </li>
-        <li>
-            <h3 class='pk-content-header'>Content header</h3>
-            <div class='pk-content'>
-				Content to collapse
-			</div>
-        </li>		
-    </ul>
+	<input type='checkbox' id='checkbox' />
 
 Javascript:
-
-	pk.accordian({
-		element: document.getElementById('accordian'),
-		animate: true,
-		multiple: true
-	});		
-		
-@class pk.accordian
+	
+	pk.checkbox({
+		element: document.getElementById('checkbox'),
+		label: 'Checked',
+		value: true
+	});
+	
+@class pk.checkbox
 @constructor
 @param options {Object}
-@param options.element {Object} DOM element to convert to component
-@param [options.animate=true] {Boolean} Animate expand/collapse actions
-@param [options.multiple=true] {Boolean} Allow multiple sections to be expanded simultaneously
-@return Object {Object} Consisting of original DOM element (item `0`)
+@param options.element {Object} DOM element to convert to control
+@param [options.value=0] {String} Value of initially selected option, defaults to the attribute value set on the passed element, or `0`
+@param [options.name=pk-checkbox-RandInt] {String} Name of underlying input control, defaults to the attribute value set on the passed element, or `pk-checkbox-RandInt`
+@param [options.label=label] {String} String to use for the control label, defaults to the attribute value set on the passed element, or its `innerHTML`
+@param [options.tabindex=0] {Number} Tabindex of control, defaults to the attribute value set on the passed element, or `0`
+@param [options.disabled=false] {Boolean} Disabled state of control, defaults to the attribute value set on the passed element, or `false`
+@param [options.listeners] {Object} Object array of event listeners to bind to underlying input(s)
+@return Object {Object} Consisting of original DOM element (item `0`) and class methods (see below)
 @chainable
 */
-
 (function(pk) {
-    pk.accordian = function(opt) {
+    pk.checkbox = function(opt) {
         var el = opt.element,
-            anim = opt.animate === false ? false : opt.animate || true,
-            multiple = opt.multiple === false ? false : opt.multiple || true;
+            //    listeners = opt.listeners === undefined ? {} : opt.listeners,
+            inputValue = opt.value || el.getAttribute('value') || 0,
+            inputLabel = opt.label || el.getAttribute('label') || el.innerHTML,
+            inputDisabled = (opt.disabled || el.getAttribute('disabled')) ? 'disabled' : '',
+            inputName = opt.name || el.getAttribute('name') || 'pk-checkbox-' + pk.getRand(1, 999),
+            listeners = opt.listeners === undefined ? {} : opt.listeners,
+            inputTabIndex = opt.tabindex || el.getAttribute('tabindex') || 0;
 
-		pk.addClass(el, 'pk-accordian');
-		
-        function animHeight(tEl) {
-            tEl.style.height = 'auto';
-            var h = pk.layout(tEl).height;
-            tEl.style.height = '0';
-            setTimeout(function() {
-                tEl.style.height = h + 'px';
-            }, 10);
-        }
+        /*jshint multistr:true */
+        var str = "<label class='pk-checkbox' for='" + inputName + "'>\
+		<input type = 'checkbox'  id = '" + inputName + "'  name = '" + inputName + "'  value = '" + inputValue + "'  tabindex = '" + inputTabIndex + "' / >\
+            <span class = 'pk-label' > " + inputLabel + " </span>\
+		</label>";
+        el.innerHTML = '';
+        el = pk.replaceEl(el, str);
+        /**
+        Gets or sets control value
+        @method val
+        @param [value] {String} Value to set
+        @return {String} Returns current value
+        */
 
-        function doLayout(tEl) {
-            for (var a = 0; a < el.children.length; a++) {
-                // loop through each....
-
-                var content = el.children[a].children[1];
-                // if multiple set to false and node passed, hide all other nodes
-                if (tEl && el.children[a] !== tEl && multiple === false) {
-                    pk.removeClass(el.children[a], 'pk-show');
+        /**
+        Gets or sets control disabled state
+        @method disabled
+        @param [boolean] {Boolean} Disabled state
+        @return {Boolean} Returns disabled state
+        */
+        var obj = {
+            0: el,
+            val: function(val) {
+                if (val === undefined) {
+                    return inputValue;
                 }
-                if (pk.hasClass(el.children[a], 'pk-show')) {
-                    // show...if not already shown
-                    if (parseInt(content.style.height, 0) === 0 || !content.style.height) {
-                        if (anim) {
-                            animHeight(content);
-                        } else {
-                            content.style.height = 'auto';
-                        }
-                    }
-                } else {
-                    // hide
-                    content.style.height = '0';
+                pk.attribute(el.children[0], 'checked', Boolean(val));
+            },
+            disabled: function(val) {
+                if (val !== undefined) {
+                    pk.toggleClass(el, 'pk-disabled', Boolean(val));
+                    pk.attribute(el.children[0], 'disabled', Boolean(val));
                 }
+                return pk.attribute(el.children[0], 'disabled');
             }
-        }
-        pk.bindEvent('click', el, function(e) {
-            if (!pk.hasClass(e.target, 'pk-content-header')) {
-                return;
-            }
-            pk.toggleClass(e.target.parentNode, 'pk-show');
-            doLayout(e.target.parentNode);
-        });
-        doLayout();
-        return {
-            0: el
         };
+        obj.val(inputValue);
+        if (inputDisabled) {
+            obj.disabled(true);
+        }
+        pk.bindListeners(listeners, el.children[0]);
+        return obj;
     };
     return pk;
 })(pk);

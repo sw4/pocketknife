bba78d2c0e2f7a2a138b4ced97881f89d61c661a
sw4 <samuel.wheeler@gmail.com>
2014-11-11 09:32:43 +0000

    Create tooltip.js

---
+++ src/tooltip/tooltip.js
@@ -0,0 +1,48 @@
+var pk = pk || {};
+(function(pk) {
+	var ttEl=null;
+	pk.tooltip=function(opt){
+		if(!ttEl){
+			ttEl=pk.createEl("<div class='pk-tooltip'></div>");
+			document.body.appendChild(ttEl);
+		}
+		pk.bindEvent('mouseover', opt.element,function(e){
+			ttEl.innerHTML=opt.content;
+			ttEl.style.display='block';
+			tl=pk.layout(ttEl);
+			ttEl.style.display='';
+			var pl=pk.layout(opt.element),
+				t=0,l=0;
+				opt.position=opt.position || 'right';
+			switch(opt.position){
+				case "top":
+					t=pl.top-tl.height-pl.height;
+					l=pl.left;
+				break;
+				case "bottom":
+					t=pl.top;
+					l=pl.left;
+				break;
+				default:
+				case "right":
+					t=pl.top-pl.height;
+					l=pl.left + pl.width;
+				break;				
+				case "left":
+					t=pl.top-pl.height;
+					l=pl.left - pl.width;
+				break;
+			}
+			ttEl.style.top=t+'px';
+			ttEl.style.left=l+'px';
+			pk.addClass(ttEl, 'pk-show');
+			pk.addClass(ttEl, 'pk-'+opt.position);			
+		});
+		pk.bindEvent('mouseout', opt.element,function(){
+			ttEl.innerHTML='';
+			pk.removeClass(ttEl, 'pk-show');			
+			pk.removeClass(ttEl, 'pk-'+opt.position);		
+		});
+	}	
+    return pk;
+})(pk);

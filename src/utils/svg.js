1f59d16a250d00ca03fa258708af62009485c871
sw4 <samuel.wheeler@gmail.com>
2014-11-13 11:26:40 +0000

    Create svg.js

---
+++ src/utils/svg.js
@@ -0,0 +1,34 @@
+var pk = pk || {};
+/**
+Utility class for SVG helpers
+
+
+@class pk.svg
+*/
+
+(function(pk) {
+    pk.svg = {
+		polar2cartesian:function(centerX, centerY, radius, angleInDegrees) {
+		  var angleInRadians = (angleInDegrees-90) * Math.PI / 180.0;
+		  return {
+			x: centerX + (radius * Math.cos(angleInRadians)),
+			y: centerY + (radius * Math.sin(angleInRadians))
+		  };
+		},
+		arcPath:function(x, y, radius, startAngle, endAngle){
+			endAngle = endAngle == 360 || endAngle > 360 ? 359.9 : endAngle ;
+			var start = this.polar2cartesian(x, y, radius, endAngle);
+			var end = this.polar2cartesian(x, y, radius, startAngle);
+
+			var arcSweep = endAngle - startAngle <= 180 ? "0" : "1";
+
+			var d = [
+				"M", start.x, start.y, 
+				"A", radius, radius, 0, arcSweep, 0, end.x, end.y
+			].join(" ");
+
+			return d;       
+		}
+    };
+    return pk;
+})(pk);

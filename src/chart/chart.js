203cce6515404013232a9c5055c424e8214c9a46
sw4 <samuel.wheeler@gmail.com>
2014-11-14 12:21:00 +0000

    Update chart.js

--- src/chart/chart.js
+++ src/chart/chart.js
@@ -1,17 +1,3 @@
-// Hello.
-//
-// This is JSHint, a tool that helps to detect errors and potential
-// problems in your JavaScript code.
-//
-// To start, simply enter some JavaScript anywhere on this page. Your
-// report will appear on the right side.
-//
-// Additionally, you can toggle specific options in the Configure
-// menu.
-
-function main() {
-  return 'Hello, World!';
-}
 var pk = pk || {};
 
 (function(pk) {
@@ -145,5 +131,3 @@ var pk = pk || {};
     };
     return pk;
 })(pk);
-
-main();

2a2485e7cdae28cd10155294baaebf7ac6e875a0
sw4 <samuel.wheeler@gmail.com>
2014-11-19 08:27:54 +0000

    Update tooltip.js

--- src/tooltip/tooltip.js
+++ src/tooltip/tooltip.js
@@ -2,10 +2,14 @@ var pk = pk || {};
 /**
 Class used for creating tooltips
 
+<div class='info-well'>
+The value passed to the `position` attribute is added to the tooltip as a CSS class of the format `pk-*position*`. Custom positions containing the keywords `top`, `left`, `bottom` and/or `right` can be applied to assume the relevant attributes, i.e. `bottomright`
+</div>
+
 HTML
 
     <span id='tooltip'>Show Tooltip</span>
-
+	
 Javascript:
 
 	pk.tooltip({
@@ -35,32 +39,26 @@ Javascript:
 		var delay=opt.delay || 500, timer=null;
 		pk.bindEvent('mouseover', opt.element,function(){
 			ttEl.innerHTML=opt.content;
-			ttEl.style.display='block';	
+			ttEl.style.display='block';				
 			var tl=pk.layout(ttEl),
 				pl=pk.layout(opt.element),
 				t=0,l=0, o={x:(opt.offset ? (opt.offset.x ? opt.offset.x : 0) : 0),y:(opt.offset ? (opt.offset.y ? opt.offset.y : 0) : 0)};
-				opt.position=opt.position || 'right';		
-			ttEl.style.display='';
 			
-			switch(opt.position){
-				case "top":
-					t=pl.top-tl.height;
-					l=pl.left;
-				break;
-				case "bottom":
-					t=pl.top+pl.height;
-					l=pl.left;
-				break;
-				default:
-				case "right":
-					t=pl.top;
-					l=pl.left + pl.width;
-				break;				
-				case "left": 
-					t=pl.top;
-					l=pl.left - tl.width;
-				break;
-			} 
+			opt.position=opt.position || 'right';				
+			ttEl.style.display='';			
+			
+			t=pl.top;
+			l=pl.left;
+			if(opt.position.indexOf("top")>-1){
+				t=pl.top-tl.height;
+			}else if(opt.position.indexOf("bottom")>-1){
+				t=pl.top+pl.height;
+			}else if(opt.position.indexOf("right")>-1){
+				l=pl.left + pl.width;
+			}else if(opt.position.indexOf("left")>-1){
+				l=pl.left - tl.width;
+			}
+			
 			ttEl.style.top=t+o.y+'px';
 			ttEl.style.left=l+o.x+'px';
 			pk.addClass(ttEl, 'pk-'+opt.position);	
